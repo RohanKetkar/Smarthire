60f63aebade28511c837ac0628ae5387935a5373
Rohan <you@example.com>
2026-03-19 10:04:44 +0530

    initial commit

--- src/test/java/com/example/Smarthire/SmarthireApplicationTests.java
+++ src/test/java/com/example/Smarthire/SmarthireApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.Smarthire;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SmarthireApplicationTests {
 
 	@Test
